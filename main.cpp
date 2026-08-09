975c25ad1127a165f2bbf0677d274b38b240ddf0
Fanghw <fanghw1507@163.com>
2026-08-10 00:20:30 +0800

    windows修改为从大小排序

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ void sort(int arr[], int size)
         // j从0开始，相邻比较
         for(int j = 0; j < size - 1 - i; j++)
         {
-            if(arr[j] > arr[j+1])
+            // windows修改为从大到小排序
+            if(arr[j] < arr[j+1])
             {
                 swap(arr[j], arr[j+1]);
             }
